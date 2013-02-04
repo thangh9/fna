79ee862ed29c4a57bd968c953f426d72971d2939
Pham Thang <thangh9@gmail.com>
2013-02-04 21:51:51 +0700

    add bootraps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fna::Application.config.secret_token = '065290981db3d9450562101110327898db85eecad6b8ac47a6b065f42105404250c64461edac3ca6eef68b3997868d6388edfd35f59a9ea4fecbe315ee632315'
