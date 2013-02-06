990d4387fe43677c02e41821910664a243c5479e
Pham Thang <thangh9@gmail.com>
2013-02-07 00:49:02 +0700

    fix layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	session[:userid] = nil
+  	session[:user_id] = nil
     redirect_to root_url, notice: "Logged out."
   end
 end
