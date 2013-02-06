b1a6448e3162bef849fe6f96eef41925d69247dd
Pham Thang <thangh9@gmail.com>
2013-02-06 22:31:37 +0700

    add validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,16 @@ class User < ActiveRecord::Base
   has_secure_password
   
   #validate email, username
- 
   validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
-
+  validates :username, uniqueness: true, presence: true
+  #validate password
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40},
+                       :on => :create
+  validates :password, :confirmation => true,
+                       :length => {:within => 6..40},
+                       :allow_blank => false,
+                       :on => :update
   attr_accessible :email, :password, :password_confirmation, :username
 end
