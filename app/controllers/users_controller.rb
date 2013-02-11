abd286492bd1099b6d746f67d867b9b5f29fbdbc
Pham Thang <thangh9@gmail.com>
2013-02-11 15:47:08 +0700

    fix login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 	def new
-		@user = User.new
+		if current_user
+			redirect_to pages_welcome_url
+		else
+			@user = User.new
+		end
 	end
 
 	def create
 		@user = User.new(params[:user])
 		if @user.save
+			session[:user_id] = @user.id
 			#redirect after login
 			redirect_to pages_welcome_url, notice: "You're Lonely no more!"
 		else
